74add7e05bde90db2e0e003d084554996ddd8c52
Onuraslann <theeaslann1@gmail.com>
2021-09-12 15:04:31 +0300

    Join ve crud işlemleri tamamlandı

---
+++ Core/Utilities/Result/SucessResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Result
+{
+    public class SucessResult:Result
+    {
+        public SucessResult(string message):base(true,message)
+        {
+
+        }
+        public SucessResult():base(true)
+        {
+
+        }
+    }
+}
---
+++ Entities/DTOs/BookDtoDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+  public  class BookDtoDetails
+    {
+        public string Type { get; set; }
+        public int Page { get; set; }
+
+        public string BookName { get; set; }
+
+        public string WriterFirstName { get; set; }
+
+        public string WriterLastName { get; set; }
+    }
+}
